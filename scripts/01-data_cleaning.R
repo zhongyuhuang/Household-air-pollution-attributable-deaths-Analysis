3448739551c2f458c57db3dde7cd593152004f3c
zhongyu <zhongyu.huang@mail.utoronto.ca>
2022-04-06 04:22:20 -0400

    cleaned dataset

--- scripts/01-data_cleaning.R
+++ scripts/01-data_cleaning.R
@@ -1,35 +1,47 @@
 #### Preamble ####
-# Purpose: Clean the survey data downloaded from [...UPDATE ME!!!!!]
-# Author: Rohan Alexander [CHANGE THIS TO YOUR NAME!!!!]
-# Data: 3 January 2021
-# Contact: rohan.alexander@utoronto.ca [PROBABLY CHANGE THIS ALSO!!!!]
+# Purpose: Clean the data downloaded from https://stats.oecd.org/Index.aspx?DataSetCode=EXP_MORSC#
+# Author: Zhongyu Huang
+# Data: 6 April 2021
+# Contact: zhongyu.huang@mail.utoronto.ca 
 # License: MIT
 # Pre-requisites: 
-# - Need to have downloaded the ACS data and saved it to inputs/data
+# - Need to have downloaded the data and saved it to inputs/data
 # - Don't forget to gitignore it!
-# - Change these to yours
-# Any other information needed?
-
 
 #### Workspace setup ####
-# Use R Projects, not setwd().
+
 library(haven)
 library(tidyverse)
+
 # Read in the raw data. 
-raw_data <- readr::read_csv("inputs/data/raw_data.csv"
-                     )
-# Just keep some variables that may be of interest (change 
-# this depending on your interests)
-names(raw_data)
+Ap_data <- readr::read_csv("inputs/data/AIR_EMISSIONS.csv")
+De_data <- readr::read_csv("inputs/data/EXP_MORSC.csv")
 
-reduced_data <- 
-  raw_data %>% 
-  select(first_col, 
-         second_col)
-rm(raw_data)
+# Just keep some variables that may be of interest from the De_data 
+reduced_De <- 
+  De_data %>% 
+  select(Country, 
+         Variable,
+         Risk,
+         Sex,
+         Age,
+         Value,
+         Year,
+         Unit)
+# rename variable "value"
+reduced_De <- reduced_De %>% rename(NumOfDeath = Value)
+
+# select observations that are from all age range, no matter what gender.
+redecued_De <- reduced_De %>% filter(reduced_De$Age =='All')
+
          
+#rename variable "value" in Ap_data
+reduced_Ap <- Ap_data %>% rename(TT_emission = Value)
 
-#### What's next? ####
+#save two dataset in outputs/data
+write.csv(reduced_Ap, file="outputs/data/cleaned_AP.csv")
+write.csv(reduced_De, file="outputs/data/cleaned_DE.csv")
+
 
 
 
